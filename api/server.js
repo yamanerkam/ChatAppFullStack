a3c387269a3f88b65782280eb1654b5f7b3e8e1b
Kam <erkamyaman35@gmail.com>
2024-05-25 13:38:20 +0300

    socket-io-cors

--- api/server.js
+++ api/server.js
@@ -9,7 +9,11 @@ app.use(cors())
 const server = http.createServer(app)
 
 const io = new Server(server, {
-
+    cors: {
+        origin: ["http://localhost:5173", "http://192.168.1.3:5173"],
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
 })
 
 io.on('connection', (socket) => {
@@ -18,7 +22,6 @@ io.on('connection', (socket) => {
     // room
 
     // message
-
 
     // disconnect
 
