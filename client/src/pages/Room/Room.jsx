ffde78b1d9c18084035da05bb6009ebd43950965
Kam <erkamyaman35@gmail.com>
2024-05-26 01:19:17 +0300

    required-input

--- client/src/pages/Room/Room.jsx
+++ client/src/pages/Room/Room.jsx
@@ -63,6 +63,7 @@ export default function Room() {
 
                 <input
                     value={message}
+                    required
                     onChange={((e) => setMessage(e.target.value))}
                     placeholder='Type a message...'
                     type="text" />
