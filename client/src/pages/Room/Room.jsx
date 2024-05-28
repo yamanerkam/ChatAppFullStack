83cc0224710302c3fccef514e6292067034a18b3
Kam <erkamyaman35@gmail.com>
2024-05-28 14:52:22 +0300

    rendering problem is gone

--- client/src/pages/Room/Room.jsx
+++ client/src/pages/Room/Room.jsx
@@ -33,7 +33,7 @@ export default function Room() {
     const handleClick = (e) => {
 
         e.preventDefault()
-        const mes = { body: message, from: socket.id, userName, userUID }
+        const mes = { msg: message, from: socket.id, userName, userUID }
         socket.emit('sendMessage', mes, id)
         setMessages(state => [mes, ...state])
         console.log(messages)
