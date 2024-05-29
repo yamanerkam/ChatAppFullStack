9d06ad2211afa8fc65c159137be83f40c2261e38
Kam <erkamyaman35@gmail.com>
2024-05-29 20:26:21 +0300

    use-ref-remove

--- client/src/pages/Room/Room.jsx
+++ client/src/pages/Room/Room.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState, useRef } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './Room.css'
 import { useParams } from "react-router-dom";
 import { io } from 'socket.io-client';
